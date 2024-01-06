e8cfc9e29c948d686e2f8a0bbdeae0f73ef118e9
Priyanka Sharma <108454203+PRIYANKAKUMARI9@users.noreply.github.com>
2024-01-06 21:35:20 +0530

    Add files via upload

---
+++ oprators.js
@@ -0,0 +1,43 @@
+
+// arithmetic oprators addition uses
+let a=40;
+let b=5;
+let c=a+b;
+console.log(c);
+
+
+//modulus
+
+let x=10;
+let y=2;
+console.log(x%y);
+
+//increment and decrement valuse
+
+z=5;
+zz=--z + ++z + --z + --z;
+console.log(zz);
+
+
+//assign oprators
+
+let aa=5;
+let bb=3;
+console.log(aa-=bb) //aa=aa-bb (2)
+
+//comparison operators
+
+console.log(3>1);
+
+//ternary oprators
+
+let age=27;
+let see=(age>=18) ? "yes vot" : "no vote"; //output = yes vote
+
+console.log(see);
+
+
+
+let atm=16;
+let atmmake= atm>=18 ? "you can open atm" : "you can't open atm" ;
+console.log(atmmake);
